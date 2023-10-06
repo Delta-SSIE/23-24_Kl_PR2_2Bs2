2136bbf43a0fcf940285435037f3e9b1cfcd47b0
Jan Koupil <jan.koupil@gmail.com>
2023-10-06 12:54:51 +0200

    2023-10-06 OOP - Opakování

---
+++ 01-OOP-010-Role/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01_OOP_010_Role
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Role role = new Role("Modrá", -150);
+                Console.WriteLine(role);
+            }
+            catch
+            {
+                Console.WriteLine("Neplatná hodnota parametru.");
+            }
+
+            try
+            {
+                Role role = new Role("Modrá", 250);
+                role.Delka -= 100;
+                Console.WriteLine(role);
+                role.Delka -= 300;
+                Console.WriteLine(role);
+            }
+            catch
+            {
+                Console.WriteLine("Neplatná hodnota parametru.");
+            }
+        }
+    }
+}
---
+++ 01-OOP-010-Role/Role.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_010_Role
+{
+    internal class Role
+    {
+        public string Barva;
+
+        private double _delka;
+
+        public double Delka
+        {
+            get { return _delka; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                _delka = value; 
+            }
+        }
+
+
+        public Role(string barva, double delka)
+        {
+            Barva = barva;
+            Delka = delka;
+        }
+
+        public override string ToString()
+        {
+            return $"Role papíru, barva {Barva}, zbývá {Delka} cm";
+        }
+    }
+}
