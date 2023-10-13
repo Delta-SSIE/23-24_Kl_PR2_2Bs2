afb70de272f5d350455e5d1f504521fd3cb1451e
Jan Koupil <jan.koupil@gmail.com>
2023-10-13 13:07:31 +0200

    2023-10-13 OOP tester a Lanovka

---
+++ 01-OOP-040-Test-vyrobku/Tester.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_040_Test_vyrobku
+{
+    internal class Tester
+    {
+		private double _tolerance;
+
+		/// <summary>
+		/// Kladná hodnota tolerance shody výrobku v procentech
+		/// </summary>
+		public double Tolerance
+		{
+			get { return _tolerance; }
+			set		
+			{
+				if (value < 0)
+					throw new ArgumentOutOfRangeException();
+				
+				_tolerance = value; 
+			}
+		}
+
+		private Vyrobek _vzor;
+
+		public Vyrobek Vzor
+		{
+			get { return _vzor; }
+			set 
+			{
+				if (value == null)
+					throw new ArgumentNullException();
+
+				_vzor = value; 
+			}
+		}
+
+        public Tester(Vyrobek vzor, double tolerance)
+        {
+            Tolerance = tolerance;
+            Vzor = vzor;
+        }
+
+		public bool Vyhovuje(Vyrobek vyrobek)
+		{
+			double odchylka = Math.Abs(vyrobek.Rozmer - Vzor.Rozmer);
+			double odchylkaVProcentech = odchylka / Vzor.Rozmer * 100;
+			
+			if (odchylkaVProcentech > Tolerance)
+				return false;
+			else
+				return true;
+		}
+    }
+}
---
+++ 01-OOP-050-Lanovka/Lanovka.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_050_Lanovka
+{
+    internal class Lanovka
+    {
+        public int Delka { get; private set; }
+        public double Nosnost { get; private set; }
+
+        private Clovek[] _sedacky;
+        public double Zatizeni
+        {
+            get
+            {
+                double celkem = 0;
+                foreach (Clovek pasazer in _sedacky)
+                {
+                    if (pasazer != null)
+                        celkem += pasazer.Hmotnost;
+                }
+                return celkem;
+            }
+        }
+        //public double Zatizeni => _sedacky.Select(x => x == null ? 0 : x.Hmotnost).Sum();
+
+        //public bool JeVolnoDole
+        //{
+        //    get
+        //    {
+        //        if (_sedacky[0] == null)
+        //            return true;
+        //        else
+        //            return false;
+        //    }
+        //}
+        public bool JeVolnoDole => _sedacky[0] == null;
+        public bool JeVolnoNahore => _sedacky[Delka - 1] == null;
+
+
+        public Lanovka(int delka, double nosnost)
+        {
+            Delka = delka;
+            _sedacky = new Clovek[Delka];
+
+            Nosnost = nosnost;
+        }
+
+        /// <summary>
+        /// Umístí člověka na první sedačku
+        /// </summary>
+        /// <param name="pasazer"></param>
+        /// <returns>true, když pasažér nastoupil, false, když se nevejde</returns>
+        public bool Nastup(Clovek pasazer)
+        {
+            if (!JeVolnoDole)
+                return false;
+            
+            if (pasazer.Hmotnost + Zatizeni > Nosnost)
+                return false;
+
+            _sedacky[0] = pasazer;
+            return true;
+        }
+
+        public Clovek Vystup()
+        {
+            Clovek pasazer = _sedacky[Delka - 1];
+            _sedacky[Delka - 1] = null;
+            return pasazer;
+        }
+
+        public void Jed()
+        {
+            if (_sedacky[Delka - 1] != null)
+                throw new Exception("Nelze jet, nahoře někdo sedí");
+
+            //půjdu odshora, budu "přesazovat"
+            for (int i = Delka - 1; i > 0; i--)
+            {
+                _sedacky[i] = _sedacky[i - 1];
+            }
+
+            _sedacky[0] = null;
+        }
+    }
+}
