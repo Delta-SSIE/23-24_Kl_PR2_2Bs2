e8463abc29ab053b9c990704cb0a65b88bd37562
Jan Koupil <jan.koupil@gmail.com>
2024-05-03 11:41:07 +0200

    2024-05-03 ChatGPT & ContactList

---
+++ 06-WPF-10-Pexeso/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _06_WPF_10_Pexeso
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
